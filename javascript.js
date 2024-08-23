0eac877fcef7dc04213b1ab3c2d2392d14c6645b
MacCrayfish <174111969+MacCrayfish@users.noreply.github.com>
2024-08-23 16:23:24 +0100

    Fixed Code by declaring functions separately.
    Declared the outcome of the player choice and computer choice functions into variables before playing a round, added a null clause

--- javascript.js
+++ javascript.js
@@ -1,37 +1,51 @@
 
 function computerChoice(){
-    return Math.floor (Math.random() * 3);
+    let number = Math.floor (Math.random() * 3);
+    if(number === 0){
+        return "rock";
+    } else if (number === 1){
+        return "paper";
+    } else {
+        return "scissors";
+    }
 }
 
 function playerChoice(lowercase){
     if(lowercase === "rock"){
-        return 0;
+        return "rock";
     } else if(lowercase === "paper"){
-        return 1;
+        return "paper";
     } else if (lowercase === "scissors"){
-        return 2;
+        return "scissors";
     } else {
         console.log("Please input a valid option");
+        return null;
     }
 }
 
 function playRound(player, computer){
     if(player === computer){
         return "draw";
-    } else if ((player === 0 && computer === 2) || (player === 1 && computer === 0) || (player === 2 && computer === 1)) {
+    } else if ((player === "rock" && computer === "scissors") || (player === "paper" && computer === "rock") || (player === "scissors" && computer === "paper")) {
         return "win";
     } else {
         return "lose";
     }
 }
+
 let playerScore = 0;
 let computerScore = 0;
+
 let choice = prompt("Rock Paper or Scissors?");
-let lowercase = choice.toLowerCase();
+let choiceLowercase = choice.toLowerCase();
+let playerOutcome = playerChoice(choiceLowercase);
+let computerOutcome = computerChoice();
 
-
-
-
-console.log(playerChoice(lowercase));
-console.log(computerChoice());
-console.log(playRound(playerChoice, computerChoice));
+if(playerOutcome !== null){
+    console.log(playerChoice(choiceLowercase));
+    console.log(computerOutcome);
+    let result = playRound(playerOutcome, computerOutcome);
+    console.log(result);
+} else {
+    console.log("Invalid");
+}
